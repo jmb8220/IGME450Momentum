f3e65d2f7c40c8ef62862bc466c42709e7b339f7
Trevor Searns <tjs9702@rit.edu>
2022-03-31 20:11:06 -0400

    Added Pause menu function and updated controls UI

--- UntitledGrappleGame/Assets/Scripts/GameManager.cs
+++ UntitledGrappleGame/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -15,19 +16,39 @@ public class GameManager : MonoBehaviour
 
     void Awake()
     {
-        if (Instance == null) // If there is no instance already
+        if (SceneManager.GetActiveScene().buildIndex != 0)
         {
-            DontDestroyOnLoad(gameObject); // Keep the GameObject, this component is attached to, across different scenes
-            Instance = this;
-        }
-        else if (Instance != this) // If there is already an instance and it's not `this` instance
-        {
-            Destroy(gameObject); // Destroy the GameObject, this component is attached to
+            if (Instance == null) // If there is no instance already
+            {
+                DontDestroyOnLoad(gameObject); // Keep the GameObject, this component is attached to, across different scenes
+                Instance = this;
+            }
+            else if (Instance != this) // If there is already an instance and it's not `this` instance
+            {
+                Destroy(gameObject); // Destroy the GameObject, this component is attached to
+            }
         }
     }
     // Start is called before the first frame update
     void Start()
     {
         Application.targetFrameRate = 60;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P) && SceneManager.GetActiveScene().buildIndex != 0)
+        {
+            PauseGame();
+        }
+    }
+
+    private void PauseGame()
+    {
+        //Return player to main menu
+        Destroy(gameObject);
+        Instance = null;
+        Cursor.lockState = CursorLockMode.None;
+        SceneManager.LoadScene(0);
+
     }
 }
